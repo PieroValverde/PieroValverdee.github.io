ad04c17e5d775ba5cc829462f3e5f8b0d06d91a3
PieroValverde <82246976+PieroValverdee@users.noreply.github.com>
2021-05-05 21:23:11 -0500

    grafico

--- graficodebarras.py
+++ graficodebarras.py
@@ -1,12 +1,13 @@
 import matplotlib.pyplot as plt
-x1=[2,4,6,8,10]
-y1=[3,5,1,7,4]
-x2=[1,3,5,7,9]
-y2=[1,3,2,4,9]
-plt.bar(x1, y1, label="barra 1" , color="g")
-plt.bar(x2, y2, label="barra 2" , color="r")
-plt.xlabel("eje x")
-plt.ylabel("eje y")
-plt.title("grafico de barras")
+hombres = [14,25,12,56,34]
+mujeres = [22,42,21,63,99]
+edades = [0,10,20,30,40,50,60,70,80,90,100]
+plt.hist(hombres, edades, histtype='bar', rwidth=0.8,
+color='b', label='hombres')
+plt.hist(mujeres, edades, histtype='bar', rwidth=0.4,
+color='r', label='mujeres')
+plt.xlabel('Eje X: Edades')
+plt.ylabel('Eje Y: Población')
+plt.title('Histograma')
 plt.legend()
 plt.show()
